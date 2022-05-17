58f0b7d45a84822854b123140b61271ec74243c9
Vu Minh Toan <vuminhtoan2001@gmail.com>
2022-05-17 23:14:25 +0700

    app init

---
+++ script.js
@@ -0,0 +1,34 @@
+const { useState } = React;
+function App() {
+  const [gift, setGift] = useState();
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState();
+  function handelGift() {
+    const index = Math.floor(Math.random() * users.length);
+    setGift(users[index]);
+  }
+  function handleAdd() {
+    setUsers([...users, user]);
+    setUser("");
+  }
+  return (
+    <div id="wrapper">
+      <input onChange={(e) => setUser(e.target.value)} value={user} className="form__field" />
+      <button onClick={handleAdd} className="btn btn--primary btn--inside uppercase">
+        Thêm
+      </button>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
+      <h1>{gift || "Là ai / Là gì / Là thế nào đây ?"}</h1>
+      <button onClick={handelGift} className="btn btn--primary">
+        Go
+      </button>
+    </div>
+  );
+}
+
+const root = document.querySelector("#root");
+ReactDOM.render(<App />, root);
